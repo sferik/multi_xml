37c4a3961c37fd98383ddc452ab8a86f1a00565a
Erik Michaels-Ober <sferik@gmail.com>
2013-12-31 06:14:33 +0100

    Enforce code coverage minimums

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ end
 
 group :test do
   gem 'coveralls', :require => false
+  gem 'json', :platforms => [:ruby_19]
   gem 'mime-types', '~> 1.25', :platforms => [:jruby, :ruby_18]
   gem 'rspec', '>= 2.11'
   gem 'simplecov', :require => false
@@ -24,6 +25,7 @@ platforms :rbx do
   gem 'racc', '~> 1.4' # Required for testing against Nokogiri on Rubinius
   gem 'rubinius-coverage',  '~> 2.0'
   gem 'rubysl', '~> 2.0'
+  gem 'rubysl-json', '~> 2.0'
 end
 
 gemspec
--- spec/helper.rb
+++ spec/helper.rb
@@ -5,7 +5,20 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
-SimpleCov.start
+
+def jruby?(platform = RUBY_PLATFORM)
+  'java' == platform
+end
+
+def rubinius?(platform = defined?(RUBY_ENGINE) && RUBY_ENGINE)
+  'rbx' == platform
+end
+
+SimpleCov.start do
+  add_filter '/spec/'
+  percent = jruby? ? 91.29 : 91.96
+  minimum_coverage(percent)
+end
 
 require 'multi_xml'
 require 'rspec'
@@ -14,12 +27,4 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
-end
-
-def jruby?(platform = RUBY_PLATFORM)
-  "java" == platform
-end
-
-def rubinius?(platform = defined?(RUBY_ENGINE) && RUBY_ENGINE)
-  "rbx" == platform
 end
