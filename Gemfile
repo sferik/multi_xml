d2bffaf3bb48f0c150649fd9ac1b36f70aa6c76a
Erik Michaels-Ober <sferik@gmail.com>
2014-07-20 22:36:36 +0200

    Update simplecov dependency to >= 0.9

--- Gemfile
+++ Gemfile
@@ -10,21 +10,17 @@ gem 'ox', :require => nil, :platforms => :ruby
 group :development do
   gem 'kramdown'
   gem 'pry'
-  platforms :ruby_19, :ruby_20 do
-    gem 'pry-debugger'
-    gem 'pry-stack_explorer'
-  end
 end
 
 group :test do
   gem 'backports'
-  gem 'coveralls', :require => false
+  gem 'coveralls'
   gem 'json', :platforms => [:rbx, :ruby_19]
   gem 'mime-types', '~> 1.25', :platforms => [:jruby, :ruby_18]
   gem 'rest-client', '~> 1.6.0', :platforms => [:jruby, :ruby_18]
   gem 'rspec', '>= 2.14'
   gem 'rubocop', '>= 0.23', :platforms => [:ruby_19, :ruby_20, :ruby_21]
-  gem 'simplecov', :require => false
+  gem 'simplecov', '>= 0.9'
   gem 'yardstick'
 end
 
--- spec/helper.rb
+++ spec/helper.rb
@@ -1,11 +1,3 @@
-require 'simplecov'
-require 'coveralls'
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
-
 def jruby?(platform = RUBY_PLATFORM)
   'java' == platform
 end
@@ -14,10 +6,17 @@ def rubinius?(platform = defined?(RUBY_ENGINE) && RUBY_ENGINE)
   'rbx' == platform
 end
 
-SimpleCov.start do
-  add_filter '/spec/'
-  percent = jruby? ? 91.29 : 91.96
-  minimum_coverage(percent)
+if RUBY_VERSION >= '1.9'
+  require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
+
+  SimpleCov.start do
+    add_filter '/spec'
+    percent = jruby? ? 91.29 : 91.96
+    minimum_coverage(percent)
+  end
 end
 
 require 'multi_xml'
