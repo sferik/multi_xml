336341e320889e32b3e3a4740ab9958d1d37c41a
Erik Michaels-Ober <sferik@gmail.com>
2015-06-23 11:59:47 +0200

    Pass &:capitalize as an argument to collect instead of a block

--- lib/multi_xml.rb
+++ lib/multi_xml.rb
@@ -104,7 +104,7 @@ module MultiXml
       case new_parser
       when String, Symbol
         require "multi_xml/parsers/#{new_parser.to_s.downcase}"
-        @parser = MultiXml::Parsers.const_get("#{new_parser.to_s.split('_').collect { |s| s.capitalize }.join('')}")
+        @parser = MultiXml::Parsers.const_get("#{new_parser.to_s.split('_').collect(&:capitalize).join('')}")
       when Class, Module
         @parser = new_parser
       else
