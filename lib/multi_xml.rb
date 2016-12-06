a49f44234644d9952c117679706c3124a71cd7b9
Erik Michaels-Ober <sferik@gmail.com>
2016-12-05 23:18:57 -0800

    Favor a normal unless-statement over a modifier clause in a multiline statement

--- lib/multi_xml.rb
+++ lib/multi_xml.rb
@@ -14,13 +14,15 @@ module MultiXml # rubocop:disable ModuleLength
     end
   end
 
-  REQUIREMENT_MAP = [
-    ['ox', :ox],
-    ['libxml', :libxml],
-    ['nokogiri', :nokogiri],
-    ['rexml/document', :rexml],
-    ['oga', :oga],
-  ].freeze unless defined?(REQUIREMENT_MAP)
+  unless defined?(REQUIREMENT_MAP)
+    REQUIREMENT_MAP = [
+      ['ox', :ox],
+      ['libxml', :libxml],
+      ['nokogiri', :nokogiri],
+      ['rexml/document', :rexml],
+      ['oga', :oga],
+    ].freeze
+  end
 
   CONTENT_ROOT = '__content__'.freeze unless defined?(CONTENT_ROOT)
 
@@ -46,19 +48,21 @@ module MultiXml # rubocop:disable ModuleLength
     }.freeze
   end
 
-  TYPE_NAMES = {
-    'Symbol'     => 'symbol',
-    'Integer'    => 'integer',
-    'BigDecimal' => 'decimal',
-    'Float'      => 'float',
-    'TrueClass'  => 'boolean',
-    'FalseClass' => 'boolean',
-    'Date'       => 'date',
-    'DateTime'   => 'datetime',
-    'Time'       => 'datetime',
-    'Array'      => 'array',
-    'Hash'       => 'hash',
-  }.freeze unless defined?(TYPE_NAMES)
+  unless defined?(TYPE_NAMES)
+    TYPE_NAMES = {
+      'Symbol'     => 'symbol',
+      'Integer'    => 'integer',
+      'BigDecimal' => 'decimal',
+      'Float'      => 'float',
+      'TrueClass'  => 'boolean',
+      'FalseClass' => 'boolean',
+      'Date'       => 'date',
+      'DateTime'   => 'datetime',
+      'Time'       => 'datetime',
+      'Array'      => 'array',
+      'Hash'       => 'hash',
+    }.freeze
+  end
 
   DISALLOWED_XML_TYPES = %w(symbol yaml).freeze
 
