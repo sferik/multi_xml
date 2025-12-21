2a78cb3f451851a9d78ddfa03ea2bf3834028ca8
Erik Berlin <sferik@gmail.com>
2025-12-21 07:52:27 -0800

    Extract global variables into SpeedTest module

--- spec/speed.rb
+++ spec/speed.rb
@@ -20,40 +20,53 @@ rescue LoadError
   next
 end
 
-$verbose = 0
-$parsers = []
-$iterations = 10
+module SpeedTest
+  class << self
+    attr_accessor :verbose, :parsers, :iterations
+  end
+
+  self.verbose = 0
+  self.parsers = []
+  self.iterations = 10
+
+  def self.run(files)
+    files.each { |filename| benchmark_file(filename) }
+  end
+
+  def self.benchmark_file(filename)
+    xml = File.read(filename)
+    times = parsers.to_h { |p| [p, time_parser(p, xml)] }
+    times.each { |p, t| print_result(p, t, filename) }
+  end
+
+  def self.time_parser(parser_name, xml)
+    MultiXml.parser = parser_name
+    start = Time.now
+    iterations.times { MultiXml.parse(StringIO.new(xml)) }
+    Time.now - start
+  end
+
+  def self.print_result(parser, time, filename)
+    puts format("%<parser>8s took %<time>0.3f seconds to parse %<file>s %<iterations>d times.",
+      parser: parser, time: time, file: filename, iterations: iterations)
+  end
+
+  def self.detect_parsers
+    parsers << "libxml" if defined?(::LibXML)
+    parsers << "nokogiri" if defined?(::Nokogiri)
+    parsers << "ox" if defined?(::Ox)
+  end
+end
 
 opts = OptionParser.new
-opts.on("-v", "increase verbosity") { $verbose += 1 }
-opts.on("-p", "--parser [String]", String, "parser to test") { |parsers| $parsers = [parsers] }
-opts.on("-i", "--iterations [Int]", Integer, "iterations") { |iterations| $iterations = iterations }
+opts.on("-v", "increase verbosity") { SpeedTest.verbose += 1 }
+opts.on("-p", "--parser [String]", String, "parser to test") { |parser| SpeedTest.parsers = [parser] }
+opts.on("-i", "--iterations [Int]", Integer, "iterations") { |iterations| SpeedTest.iterations = iterations }
 opts.on("-h", "--help", "Show this display") do
   puts opts
   Process.exit!(0)
 end
 files = opts.parse(ARGV)
 
-if $parsers.empty?
-  $parsers << "libxml" if defined?(::LibXML)
-  $parsers << "nokogiri" if defined?(::Nokogiri)
-  $parsers << "ox" if defined?(::Ox)
-end
-
-files.each do |filename|
-  times = {}
-  xml = File.read(filename)
-  $parsers.each do |p|
-    MultiXml.parser = p
-    start = Time.now
-    $iterations.times do
-      io = StringIO.new(xml)
-      MultiXml.parse(io)
-    end
-    times[p] = Time.now - start
-  end
-  times.each do |p, t|
-    puts format("%<parser>8s took %<time>0.3f seconds to parse %<file>s %<iterations>d times.",
-      parser: p, time: t, file: filename, iterations: $iterations)
-  end
-end
+SpeedTest.detect_parsers if SpeedTest.parsers.empty?
+SpeedTest.run(files)
