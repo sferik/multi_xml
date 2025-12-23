d6f7415522ef6941bed57dc4d19427e43c5aac0c
Erik Berlin <sferik@gmail.com>
2025-12-23 07:39:19 -0800

    Remove test skips

--- test/parser_tests.rb
+++ test/parser_tests.rb
@@ -11,7 +11,8 @@ require "children_tests"
 require "mixed_attribute_tests"
 require "stream_tests"
 
-module ParserTests
+# Common tests that run on all parsers
+module ParserCommonTests
   include ParserTestSetup
   include ParserBasicTests
   include ParserWhitespaceTests
@@ -24,4 +25,22 @@ module ParserTests
   include ParserChildrenTests
   include ParserMixedAttributeTests
   include ParserStreamTests
+end
+
+# Tests for DOM parsers (all parsers except SAX variants)
+module DomParserTests
+  include ParserCommonTests
+  include ParserStrictErrorTests
+end
+
+# Tests for DOM parsers that don't raise on invalid XML (Oga)
+module LenientDomParserTests
+  include ParserCommonTests
+end
+
+# Tests for SAX parsers
+module SaxParserFullTests
+  include ParserCommonTests
+  include ParserStrictErrorTests
+  include SaxParserTests
 end
